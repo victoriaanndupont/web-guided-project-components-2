b7034ee4ecf42c30b670d2c60302490aa0685bc0
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-03 11:43:57 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -12,8 +12,12 @@ axios
   .get(`https://lambda-times-api.herokuapp.com/friends`)
   .then((futureData) => {
     // future code for when the data actually arrives
-    // freedom to assume that data is actually here
-    console.log("2. here is the future data: ", futureData);
+    // freedom to assume that the data is here
+    console.log("2. here is the future data", futureData);
+  })
+  .catch((drama) => {
+    // handle the drama
+    console.log(drama);
   });
 
 console.log("3. we requested the data with axios");

e0e1752c1e2f7c64d51bfb7262e221f0fcd892ff
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-03 11:15:02 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // We never nest imports inside blocks of code!
 import axios from "axios";
 console.log(axios);
-const result = axios.get();
+const result = axios.get("https://lambda-times-api.herokuapp.com/friends");
 
 // 👉 TASK 1- Test out the following endpoints:
 

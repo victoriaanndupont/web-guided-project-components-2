7977484baea0af436028167578621c869e718388
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-03 11:09:18 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Imports at the top of the file!
 // We never nest imports inside blocks of code!
-
+import axios from "axios";
 
 // 👉 TASK 1- Test out the following endpoints:
 
@@ -16,54 +16,47 @@
 //  * With Chrome and the Network Tab
 //  * With JS using the native fetch [STRETCH]
 
-
 // 👉 TASK 2- Select the "entry point", the element
-// inside of which we'll inject our dog cards 
-const entryPoint = null
-
+// inside of which we'll inject our dog cards
+const entryPoint = null;
 
 // 👉 TASK 3- `dogCardMaker` takes an object and returns a Dog Card.
 // Use this function to build a Card, and append it to the entry point.
 function dogCardMaker({ imageURL, breed }) {
   // instantiating the elements
-  const dogCard = document.createElement('div')
-  const image = document.createElement('img')
-  const heading = document.createElement('h3')
+  const dogCard = document.createElement("div");
+  const image = document.createElement("img");
+  const heading = document.createElement("h3");
   // setting class names, attributes and text
-  heading.textContent = `Breed: ${breed}`
-  image.src = imageURL
-  image.classList.add('dog-image')
-  dogCard.classList.add('dog-card')
+  heading.textContent = `Breed: ${breed}`;
+  image.src = imageURL;
+  image.classList.add("dog-image");
+  dogCard.classList.add("dog-card");
   // creating the hierarchy
-  dogCard.appendChild(image)
-  dogCard.appendChild(heading)
+  dogCard.appendChild(image);
+  dogCard.appendChild(heading);
   // adding some interactivity
-  dogCard.addEventListener('click', () => {
-    dogCard.classList.toggle('selected')
-  })
+  dogCard.addEventListener("click", () => {
+    dogCard.classList.toggle("selected");
+  });
   // never forget to return!
-  return dogCard
+  return dogCard;
 }
-
 
 // 👉 TASK 4- Bring the Axios library into the project using one of two methods:
 //    * Traditional way: put another script tag inside index.html (`https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js`)
 //    * Proyects with npm: install it with npm and import it into this file
-
 
 // 👉 TASK 5- Fetch dogs from `https://dog.ceo/api/breed/{breed}/images/random/{number}`
 //    * ON SUCCESS: use the data to create dogCards and append them to the entry point
 //    * ON FAILURE: log the error to the console
 //    * IN ANY CASE: log "done" to the console
 
-
 // 👉 (OPTIONAL) TASK 6- Wrap the fetching operation inside a function `getDogs`
 // that takes a breed and a count (of dogs)
 
-
 // 👉 (OPTIONAL) TASK 7- Put a button in index.html to 'get dogs' and add a click
 // event listener that executes `getDogs`
-
 
 // 👉 (OPTIONAL) TASK 8- Import the breeds from `breeds.js`
 // or request them from https://lambda-times-api.herokuapp.com/breeds

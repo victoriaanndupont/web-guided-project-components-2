5ead50f3e6225135a076a4c46791f90c054e88f1
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-03 12:22:31 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -72,6 +72,14 @@ function dogCardMaker({ imageURL, breed }) {
 //    * ON FAILURE: log the error to the console
 //    * IN ANY CASE: log "done" to the console
 
+axios
+  .get(`https://dog.ceo/api/breed/retriever/images/random/5`)
+  .then((res) => {
+    debugger;
+  })
+  .catch((err) => {
+    debugger;
+  });
 // 👉 (OPTIONAL) TASK 6- Wrap the fetching operation inside a function `getDogs`
 // that takes a breed and a count (of dogs)
 
